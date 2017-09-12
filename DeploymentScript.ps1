a980e4e529c9b93c92a8fe03ca65eafba48b222f
Emil Claussen <Emil.Claussen@lanekassen.no>
2017-09-12 20:13:46 +0200

    Added script output and load balancer to ARM template

--- DeploymentScript.ps1
+++ DeploymentScript.ps1
@@ -8,13 +8,18 @@ $location = "West Europe"
 <# 
     Create resource group and deploy ARM template to the resource group.
 #>
+Write-Host -ForegroundColor Yellow "Starting ARM template deployment.."
 $rg = New-AzureRmResourceGroup -Name $rg -Location $location
-New-AzureRmResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -TemplateFile "C:\Users\slk0emcl\Desktop\MSU\Prosjekt\azuredeployment.json" -Verbose -ErrorAction Stop
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName -TemplateFile "C:\Users\slk0emcl\Desktop\contoso-template-azure\ARM\azuredeployment.json" -Verbose -ErrorAction Stop
+Write-Host -ForegroundColor Green "ARM template was deployed!"
 
 <# 
     Get VPN gateway public IP and add it to on-premise RRAS gateway using Azure Script Extension.
 #>
+Write-Host -ForegroundColor Yellow "Getting IP for Azure VPN gateway.."
 $gwyPip = Get-AzureRmPublicIpAddress -Name gatewayPip -ResourceGroupName $rg.ResourceGroupName
+Write-Host -ForegroundColor Green "VPN gateway is on $($gwyPip.IpAddress)!"
+Write-Host -ForegroundColor Yellow "Registering Azure VPN endpoint to on-premise RRAS VPN gateway.."
 $extension = Set-AzureRmVMCustomScriptExtension -ResourceGroupName OnPremiseRG `
     -VMName DC1 `
     -Location $location `
@@ -22,6 +27,7 @@ $extension = Set-AzureRmVMCustomScriptExtension -ResourceGroupName OnPremiseRG `
     -Run "SetVpnInterface.ps1" `
     -Argument "$($gwyPip.IpAddress)" `
     -Name UpdateS2SInterface
+if($extension.IsSuccessStatusCode){Write-Host -ForegroundColor Green "Azure VPN endpoint was successfully registered!"}
 
 <# 
     Define domain info and configuration required to run DscCompilationJob
@@ -45,31 +51,50 @@ $cd = @{
 <# 
     Compile DSC configurations using parameters and configuration data
 #>
+$wsConfig = "WebServer"
+Write-Host -ForegroundColor Yellow "Starting DSC compile job for $($wsConfig).."
 $iisJob = Start-AzureRmAutomationDscCompilationJob -ResourceGroupName $automationRg `
-    -AutomationAccountName "ContosoAutomation" -ConfigurationName "WebServer" `
+    -AutomationAccountName "ContosoAutomation" -ConfigurationName $wsConfig `
     -Parameters $Parameters -ConfigurationData $cd
 
+$dcConfig = "DomainController"
+Write-Host -ForegroundColor Yellow "Starting DSC compile job for $($dcConfig).."
 $dcJob = Start-AzureRmAutomationDscCompilationJob -ResourceGroupName $automationRg `
-    -AutomationAccountName "ContosoAutomation" -ConfigurationName "DomainController" `
+    -AutomationAccountName "ContosoAutomation" -ConfigurationName $dcConfig `
     -Parameters $Parameters -ConfigurationData $cd
 
 do {
+    Write-Host -ForegroundColor Yellow "Waiting for compilation job.."
+    Write-Host -ForegroundColor Cyan "$($wsConfig) job: $($iisJob.Status) `n$($dcConfig) job: $($dcJob.Status)"
     $iisJob = $iisJob | Get-AzureRmAutomationDscCompilationJob
     $dcJob = $dcJob | Get-AzureRmAutomationDscCompilationJob
-    Start-Sleep -s 10
-    Write-Output "Venter 10 sekunder på jobbstatus..."
+    Start-Sleep -s 30
+    Write-Output "Venter 15 sekunder på jobbstatus..."
 } while ($iisJob.Status -ne "Completed" -or $dcJob.Status -ne "Completed")
+Write-Host -ForegroundColor Green "DSC compilation jobs completed successfully!"
 
 <# 
     Register DSC configurations with corresponding VMs
 #>
-$webAvailabilitySet = @("Web1", "Web2") | % {Register-AzureRmAutomationDscNode `
+$webAvailabilitySet = @("Web1", "Web2") | % {
+    Write-Host -ForegroundColor Yellow "Registering Azure VM $($_.ToString()) as DSC node using the $($wsConfig) configuration.."
+
+    $registration = Register-AzureRmAutomationDscNode `
     -ResourceGroupName $automationRg -AzureVMName $_.ToString() `
     -NodeConfigurationName "WebServer.localhost" -ConfigurationMode ApplyAndMonitor `
-    -AutomationAccountName ContosoAutomation -AzureVMResourceGroup $rg.ResourceGroupName }
+    -AutomationAccountName ContosoAutomation -AzureVMResourceGroup $rg.ResourceGroupName
+    
+    if ($?){Write-Host -ForegroundColor Green "$($_.ToString()) was successfully registered as a DSC node!"}
+}
 
-$dcAvailabilitySet = @("DC2", "DC3") | % { Register-AzureRmAutomationDscNode `
+$dcAvailabilitySet = @("DC2", "DC3") | % { 
+    Write-Host -ForegroundColor Yellow "Registering Azure VM $($_.ToString()) as DSC node using the $($dcConfig) configuration.."
+
+    $registration = Register-AzureRmAutomationDscNode `
     -ResourceGroupName $automationRg -AzureVMName $_.ToString() `
     -NodeConfigurationName "DomainController.localhost" -ActionAfterReboot ContinueConfiguration `
      -RebootNodeIfNeeded $true -ConfigurationMode ApplyAndMonitor `
-     -AutomationAccountName ContosoAutomation -AzureVMResourceGroup $rg.ResourceGroupName }
+     -AutomationAccountName ContosoAutomation -AzureVMResourceGroup $rg.ResourceGroupName 
+     
+     if ($?){Write-Host -ForegroundColor Green "$($_.ToString()) was successfully registered as a DSC node!"}
+}
